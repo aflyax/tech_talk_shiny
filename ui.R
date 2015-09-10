f880d1869e8ac495ff9c6b5833a08e2b2e4efaf6
Alexander Flyax <aflyax@ch-nb-725.local>
2015-09-09 19:32:44 -0500

    first step: make selectizeInput for picking X (names of diamonds dataset)

--- ui.R
+++ ui.R
@@ -2,7 +2,23 @@ shinyUI(fluidPage(
   headerPanel(
     'Shiny Tech Talk'
   ),
-  sidebarPanel('sidebar'),
+  sidebarPanel('sidebar',
+               selectizeInput(inputId = 'select_X',
+                              label = 'please select X',
+                              choices = names(dataset),
+                              select = names(dataset)[1:2],
+                              multiple = TRUE
+               )
+               
+               
+               
+               
+               
+  ),
+  
+  
+  
+  
   mainPanel('mainbar')
   
 )
