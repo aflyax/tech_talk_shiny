a90e967b29fa5c1ae322e6be81fac200a5ececf9
Alexander Flyax <aflyax@ch-nb-725.local>
2015-09-09 19:38:30 -0500

    Step II: make more panels

--- ui.R
+++ ui.R
@@ -15,9 +15,24 @@ shinyUI(fluidPage(
     tabsetPanel(
       tabPanel(inputId = 'raw_data',
                title = 'raw data'
-        
+      ),
+      
+      tabPanel(inputId = 'selected_data',
+               title = 'selected data'
+      ),
+      
+      tabPanel(inputId = 'data_plot',
+               title = 'plot data'
+      ),
+      
+      tabPanel(inputId = 'model_output',
+               title = 'model output'
+      ),
+      
+      tabPanel(inputId = 'model_plot',
+               title = 'model plot'
       )
-    )       
+    )
   )
   
 )
